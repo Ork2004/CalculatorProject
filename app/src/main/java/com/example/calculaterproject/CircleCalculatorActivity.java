bfe8e33cdbf2b13d3db32042c0eecb9fd477a46b
Ork2004 <120731187+Ork2004@users.noreply.github.com>
2025-02-08 23:06:44 +0500

    Added calculator UI and initialized element for circle calculator

--- app/src/main/java/com/example/calculaterproject/CircleCalculatorActivity.java
+++ app/src/main/java/com/example/calculaterproject/CircleCalculatorActivity.java
@@ -2,15 +2,24 @@ package com.example.calculaterproject;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class CircleCalculatorActivity extends AppCompatActivity {
+    private EditText radiusInput;
+    private Button btnCalculate, btnReset;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_circle_calculator);
+
+        radiusInput = findViewById(R.id.radiusInput);
+        btnCalculate = findViewById(R.id.btnCalculate);
+        btnReset = findViewById(R.id.btnReset);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
