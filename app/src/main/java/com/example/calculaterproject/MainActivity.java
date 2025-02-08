b689fce12fa12eec16c130a36292e0c0b324f294
Ork2004 <120731187+Ork2004@users.noreply.github.com>
2025-02-08 22:38:10 +0500

    Added calculator UI and initialized elements

--- app/src/main/java/com/example/calculaterproject/MainActivity.java
+++ app/src/main/java/com/example/calculaterproject/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.calculaterproject;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +12,22 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText input1, input2;
+    private Button btnAdd, btnSubtract, btnMultiply, btnDivide, btnReset;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        input1 = findViewById(R.id.input1);
+        input2 = findViewById(R.id.input2);
+        btnAdd = findViewById(R.id.btnAdd);
+        btnSubtract = findViewById(R.id.btnSubtract);
+        btnMultiply = findViewById(R.id.btnMultiply);
+        btnDivide = findViewById(R.id.btnDivide);
+        btnReset = findViewById(R.id.btnReset);
 
     }
 }
